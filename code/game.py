a963715b6e59daedc640f97365e0a7824c2f81d3
Gabriel Bueno <gabrielrosabueno@hotmail.com>
2025-06-30 20:28:58 -0300

    Criando main loop e Conectando as classes de Menu e Game

--- code/game.py
+++ code/game.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from code.menu import Menu
+import pygame
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode((800, 600))
 
-    def run(self, ):
-        pass
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import pygame
-
-print("Setup started...")
-pygame.init()
-window = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("My Pygame Window")
-print("Pygame initialized and window created.")
+from code.game import Game
 
 
-running = True
-while running:  
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    window.fill((0, 0, 0))  # Fill the window with black
-    pygame.display.flip()  # Update the display 
+game = Game()
+game.run()
